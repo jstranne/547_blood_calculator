682ff07762eb7f41eeba6f1bb508893755be0621
Jason Stranne <jasonstranne@gmail.com>
2020-08-24 13:08:59 -0400

    added a function to analyze the range that the HDL result is in

--- calculator.py
+++ calculator.py
@@ -1,27 +1,38 @@
 def interface():
-	print("My Program")
-	while(True):
-		print("Options:")
-		print("1 - HDL")
-		print("9 - Quit")
-		choice = input("Enter your choice: ")
-		if choice=='9':
-			return
-		elif choice=='1':
-			HDL_driver()
+    print("My Program")
+    while(True):
+        print("Options:")
+        print("1 - HDL")
+        print("9 - Quit")
+        choice = input("Enter your choice: ")
+        if choice == '9':
+            return
+        elif choice == '1':
+            HDL_driver()
+
 
 def HDL_driver():
 	# Get input
 	hdl = input("Enter your HDL: ")
-	#check if hdl is normal
-
+	# check if hdl is normal
+	result = check_hdl_range(hdl)
 	# output results
+	
+	# check if Low
 
-	#check if Low
 
 def get_HDL_input():
-	HDL_input = input("Enter your HDL result: ")
-	return int(HDL_input)
-	
+    HDL_input = input("Enter your HDL result: ")
+    return int(HDL_input)
+
+
+def check_hdl_range(hdl):
+    if(hdl >= 60):
+        return "Normal"
+    elif(hdl >= 40):
+        return "Borderline Low"
+    else:
+        return "Low"
+
 
 interface()
