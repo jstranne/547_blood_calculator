1330327e441a20c297f39f5db328b6036a1273cb
Jason Stranne <jasonstranne@gmail.com>
2020-08-24 13:06:04 -0400

    added the ability for the user to input their HDL level

--- calculator.py
+++ calculator.py
@@ -2,9 +2,26 @@ def interface():
 	print("My Program")
 	while(True):
 		print("Options:")
+		print("1 - HDL")
 		print("9 - Quit")
 		choice = input("Enter your choice: ")
 		if choice=='9':
 			return
+		elif choice=='1':
+			HDL_driver()
+
+def HDL_driver():
+	# Get input
+	hdl = input("Enter your HDL: ")
+	#check if hdl is normal
+
+	# output results
+
+	#check if Low
+
+def get_HDL_input():
+	HDL_input = input("Enter your HDL result: ")
+	return int(HDL_input)
+	
 
 interface()
